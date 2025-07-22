18a5fae8eb74bfbb8201c77bedd35e79ae4b624b
Joshua Lima <joxalima29@gmail.com>
2025-07-21 21:50:13 -0300

    ajustes no repositorio

--- Data/AlunoRepository.cs
+++ Data/AlunoRepository.cs
@@ -18,7 +18,7 @@ namespace BibliotecaMVC.Data
         public List<Aluno> Listar( ) {
             List<Aluno> alunos = new List<Aluno>
             {
-                new Aluno("Ana Silva", "2023001", "ana.silva@email.com", "(11) 91234-5678", new DateTime(2000, 5, 15)),
+                new Aluno("Ana Silva Pereira", "2023001", "ana.silva@email.com", "(11) 91234-5678", new DateTime(2000, 5, 15)),
                 new Aluno("Bruno Costa", "2023002", "bruno.costa@email.com", "(11) 92345-6789", new DateTime(1999, 8, 22)),
                 new Aluno("Carla Oliveira", "2023003", "carla.oliveira@email.com", "(11) 93456-7890", new DateTime(2001, 3, 10)),
                 new Aluno("Daniela Pereira", "2023004", "daniela.pereira@email.com", "(11) 94567-8901", new DateTime(2002, 12, 5)),
@@ -30,6 +30,7 @@ namespace BibliotecaMVC.Data
                 new Aluno("Juliana Ferreira", "2023010", "juliana.ferreira@email.com", "(11) 99123-4567", new DateTime(1996, 6, 20))
             };
             return alunos ?? [];
+            //teste
         }
     }
         
